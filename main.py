7e638b1868b2cce0c3f8b283b62e62241abadc33
Wojolan <109399275+aa901002x@users.noreply.github.com>
2024-09-29 11:30:52 +0800

    add weng1002 homework2

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# HW2 - test1
 @app.get("/")
 async def root():
     return {
             "message": "Hello World"
             }
 
+
+# HW2 - test2
+@app.get("/items/{item_id}")
+async def read_item(item_id : int, q : str|None = None) :
+    results = {
+        "item_id": item_id,
+        "name": "Test Item",
+        "description": "A test description",
+        "price": 10.5,
+        "tax": 1.5
+    }
+    if q:
+        results.update({"q":q})
+    return results
