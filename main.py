e16139d1cdeb2e3a4383e1b9b0623870a638b2e6
Wojolan <109399275+aa901002x@users.noreply.github.com>
2024-10-16 22:39:50 +0800

    Add weng1002 homework 3

--- main.py
+++ main.py
@@ -26,33 +26,38 @@ async def read_item(item_id : int, q : str|None = None) :
     return results
 
 
-# # HW3 - test1
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: Annotated[int, Path(gt=1, lt=1000)], q: Annotated[str | None, Query(max_length=50, min_length=3)] = None, sort_order: str = "asc"):
-#     if q:
-#         description = f"This is a sample item that matches the query {q}."
-#     else:
-#         description = "This is a sample item."
+# HW3 - test1
+@app.get("/items/{item_id}")
+async def read_item(item_id: Annotated[int, Path(ge=1, le=1000)], q: Annotated[str | None, Query(max_length=50, min_length=3)] = None, sort_order: str | None = "asc"):
+    if q:
+        description = f"This is a sample item that matches the query {q}."
+    else:
+        description = "This is a sample item."
     
-#     results = { 
-#         "item_id": item_id,
-#         "description": description,
-#         "sort_order": sort_order
-#     }
-#     return results
+    results = { 
+        "item_id": item_id,
+        "description": description,
+        "sort_order": sort_order
+    }
+    return results
 
-# # HW3 - test12
-# @app.put("/items/{item_id}")
-# async def update_item(item_id: Annotated[int, Path(gt=1, lt=1000)], name: str,desription: str = None, price: float = None, tax: float = None,q: Annotated[str | None, Query(max_length=50, min_length=3)] = None):
-#     results = {
-#         "item_id": item_id,
-#         "name": name,
-#         "description": desription,
-#         "price": price,
-#         "tax": tax
-#     }
-#     if q:
-#         results.update({"q": q})
+# HW3 - test12
+from pydantic import BaseModel
 
-#     return results
+class Item(BaseModel):
+    name: str
+    description: str
+    price: float
+    tax: float
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: Annotated[int, Path(ge=1, le=1000)],item: Item, q: Annotated[str | None, Query(max_length=50, min_length=3)] = None):
+    results = {
+        "item_id": item_id,
+        **item.dict()
+    }
+    if q:
+        results.update({"q": q})
+
+    return results
 
